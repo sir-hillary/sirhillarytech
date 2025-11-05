e9d086fdbe868b77f02393142b4853822cafa12f
Hillary Omondi <h.o.omondi15@gmail.com>
2025-11-05 21:18:34 +0300

    commit after addition of two new projects voila!!🎉

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,103 +1,8 @@
 // Portfolio.jsx
 import { useState } from "react";
 import { Code, Palette, Github, ExternalLink } from "lucide-react";
-import sandwich_img from "../../assets/sandwich_project.png";
-import nyamaverse from "../../assets/nyamaverse_project.png";
-import burger from "../../assets/burger-project.png";
-import klock_logo from "../../assets/klock_project.webp";
-import minimal_logo from "../../assets/minimalistic-logo-project.webp";
-import velocity_logo from "../../assets/velocityfitness_design.png";
-import event_banner from "../../assets/youthconvention_banner.png";
-import ava_logo from "../../assets/AVA_logo.webp"
+import { projects } from "../../assets/assets";
 
-const projects = {
-  development: [
-    {
-      title: "A movie ticketing app BACKEND API.",
-      description:
-        "A movie ticketing app api that controls authentication, adding new shows, controls booking of the show, role based authentication and many more features.",
-      link: "https://github.com/sir-hillary/movie-ticket-booking/",
-      demo: "https://quick-show-psi-seven.vercel.app/",
-    },
-    {
-      title: "SMARTGPT AI Chatbot Clone – Fullstack MERN Application",
-      description:
-      "Create images and text content with this AI clone made with MERN stack technologies. Its fast and responsive with all devices",
-      link: "https://github.com/sir-hillary/SmartGPT.git",
-      demo: "https://smart-gpt-swart.vercel.app",
-    },
-    {
-      title: "Expense-Tracker app",
-      description: "Track Expenses easily using UI and localstorage.",
-      link: "https://github.com/sir-hillary/expense-tracker.git",
-      demo: "https://sir-hillary.github.io/expense-tracker/",
-    },
-    {
-      title: "SmartBlog – Create Smarter, Blog Faster",
-      description: "SmartBlog is an AI-powered blogging platform that helps creators write and publish blogs effortlessly. It saves time, boosts creativity, and makes content creation simple for bloggers, brands, and marketers alike.",
-      link: "https://github.com/sir-hillary/SMARTBLOG.git",
-      demo: "https://smartblog-inky.vercel.app",
-    },
-     {
-      title: "Mkulima Connect Africa – Growing Digital Agriculture",
-      description: "Mkulima Connect Africa is a professional client landing page designed to connect farmers, buyers, and agri-entrepreneurs across Africa. It promotes agricultural visibility, fosters trust, and bridges the gap between farmers and the digital market.",
-      link: "https://github.com/sir-hillary/Mkulima-Connect-Project.git",
-      demo: "https://mkulimaconnectafrica.netlify.app/",
-    },
-     {
-      title: "Agency.ai – Digital Marketing Website",
-      description: "A modern, responsive single-page website built for digital marketing agencies to attract clients and boost online visibility. Designed to convert visitors into leads through sleek visuals, clear messaging, and strong calls-to-action.",
-      link: "https://github.com/sir-hillary/agency-ai.git",
-      demo: "https://agency-ai-gamma-six.vercel.app",
-    },
-
-  ],
-  design: [
-    {
-      title: "SandWich Ad Poster",
-      description: "A Canva-designed sandwich poster that markets the brand.",
-      image: sandwich_img,
-    },
-    {
-      title: "Restaurant Logo Design",
-      description:
-        "Professional real restaurant logo that is meant to picture its quality in offering food.",
-      image: nyamaverse,
-    },
-    {
-      title: "Burger Ad Poster",
-      description:
-        "Building your brand with nice posters for social media marketing.",
-      image: burger,
-    },
-    {
-      title: "Klock Cigar Logo",
-      description: "Clean, minimal and aesthetic logo.",
-      image: klock_logo,
-    },
-    {
-      title: "A super cool minimalistic logo.",
-      description: "Building your brand with nice logos to match your brand.",
-      image: minimal_logo,
-    },
-    {
-      title: "VelocityFitnessGym Logo",
-      description:
-        "Building your fitness brand with a beautiful and sleek logo.",
-      image: velocity_logo,
-    },
-    {
-      title: "A banner for for a group event.",
-      description: "Building events banners to unite and attract audience.",
-      image: event_banner,
-    },
-     {
-      title: "A really cool but simple logo of AVA brand",
-      description: "Creating logo that outshines your expectation.",
-      image: ava_logo,
-    },
-  ],
-};
 
 export default function Portfolio() {
   const [activeTab, setActiveTab] = useState("development");
@@ -171,6 +76,7 @@ export default function Portfolio() {
                     <Github className="w-5 h-5" />
                   </a>
                 </div>
+                {project.completed ? <p className="text-sm bg-green-400 text-white px-4 py-2 rounded-md w-32 mt-4">Completed</p> : <p className="text-sm text-orange-400">Ongoing</p>}
               </div>
             ))}
           </div>
---
+++ src/assets/assets.js
@@ -0,0 +1,122 @@
+import sandwich_img from "./sandwich_project.png";
+import nyamaverse from "./nyamaverse_project.png";
+import burger from "./burger-project.png";
+import klock_logo from "./klock_project.webp";
+import minimal_logo from "./minimalistic-logo-project.webp";
+import velocity_logo from "./velocityfitness_design.png";
+import event_banner from "./youthconvention_banner.png";
+import ava_logo from "./AVA_logo.webp";
+
+export const projects = {
+  development: [
+    {
+      title: "SMARTGPT AI Chatbot Clone – Fullstack MERN Application",
+      description:
+        "Create images and text content with this AI clone made with MERN stack technologies. Its fast and responsive with all devices",
+      link: "https://github.com/sir-hillary/SmartGPT.git",
+      demo: "https://smart-gpt-swart.vercel.app",
+      completed: true
+    },
+
+    {
+      title: "SmartBlog – Create Smarter, Blog Faster",
+      description:
+        "SmartBlog is an AI-powered blogging platform that helps creators write and publish blogs effortlessly. It saves time, boosts creativity, and makes content creation simple for bloggers, brands, and marketers alike.",
+      link: "https://github.com/sir-hillary/SMARTBLOG.git",
+      demo: "https://smartblog-inky.vercel.app",
+      completed: true
+    },
+    {
+      title: "Mkulima Connect Africa – Growing Digital Agriculture",
+      description:
+        "Mkulima Connect Africa is a professional client landing page designed to connect farmers, buyers, and agri-entrepreneurs across Africa. It promotes agricultural visibility, fosters trust, and bridges the gap between farmers and the digital market.",
+      link: "https://github.com/sir-hillary/Mkulima-Connect-Project.git",
+      demo: "https://mkulimaconnectafrica.netlify.app",
+      completed: true
+    },
+    {
+      title: "Agency.ai – Digital Marketing Website",
+      description:
+        "A modern, responsive single-page website built for digital marketing agencies to attract clients and boost online visibility. Designed to convert visitors into leads through sleek visuals, clear messaging, and strong calls-to-action.",
+      link: "https://github.com/sir-hillary/agency-ai.git",
+      demo: "https://agency-ai-gamma-six.vercel.app",
+      completed: true
+    },
+    {
+      title: "GreenCart – Smart Grocery Shopping Made Easy",
+      description:
+        "A fullstack MERN e-commerce platform that helps customers order fresh groceries quickly and conveniently from local vendors, saving time while ensuring quality and accessibility.",
+      link: "https://github.com/sir-hillary/greencart-ecommerce-web-app",
+      demo: "https://greencartecommerce.vercel.app",
+      completed: false
+    },
+    {
+      title: "MediTrack – Pharmacy Management System",
+      description:
+        "A full-stack MERN web app that helps pharmacy owners and staff efficiently manage drug inventory, sales, and transactions in real time. MediTrack simplifies daily operations with smart analytics, role-based access, and an intuitive dashboard interface.",
+      link: "https://github.com/sir-hillary/meditrack",
+      demo: "https://jackypharmaceuticals.vercel.app",
+      completed: true
+    },
+    {
+      title: "A movie ticketing app BACKEND API.",
+      description:
+        "A movie ticketing app api that controls authentication, adding new shows, controls booking of the show, role based authentication and many more features.",
+      link: "https://github.com/sir-hillary/movie-ticket-booking/",
+      demo: "https://quick-show-psi-seven.vercel.app",
+      completed: false
+    },
+    {
+      title: "Expense-Tracker app",
+      description: "Track Expenses easily using UI and localstorage.",
+      link: "https://github.com/sir-hillary/expense-tracker.git",
+      demo: "https://sir-hillary.github.io/expense-tracker/",
+      completed: false
+    },
+  ],
+  design: [
+    {
+      title: "SandWich Ad Poster",
+      description: "A Canva-designed sandwich poster that markets the brand.",
+      image: sandwich_img,
+    },
+    {
+      title: "Restaurant Logo Design",
+      description:
+        "Professional real restaurant logo that is meant to picture its quality in offering food.",
+      image: nyamaverse,
+    },
+    {
+      title: "Burger Ad Poster",
+      description:
+        "Building your brand with nice posters for social media marketing.",
+      image: burger,
+    },
+    {
+      title: "Klock Cigar Logo",
+      description: "Clean, minimal and aesthetic logo.",
+      image: klock_logo,
+    },
+    {
+      title: "A super cool minimalistic logo.",
+      description: "Building your brand with nice logos to match your brand.",
+      image: minimal_logo,
+    },
+    {
+      title: "VelocityFitnessGym Logo",
+      description:
+        "Building your fitness brand with a beautiful and sleek logo.",
+      image: velocity_logo,
+    },
+    {
+      title: "A banner for for a group event.",
+      description: "Building events banners to unite and attract audience.",
+      image: event_banner,
+    },
+    {
+      title: "A really cool but simple logo of AVA brand",
+      description: "Creating logo that outshines your expectation.",
+      image: ava_logo,
+    },
+  ],
+};
